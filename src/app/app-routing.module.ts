fb05019442b62068871dc7b97897c985521828c8
Ayush Anand <ayush.a@testyantra.in>
2019-10-24 19:06:30 +0530

    aa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { IndustriesComponent } from './industries/industries.component';
+import { DigitalComponent } from './digital/digital.component';
+import { ContactComponent } from './contact/contact.component';
+import { LeadershipComponent } from './leadership/leadership.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '' , component : HomeComponent },
+  {path : 'service' , component : ServiceComponent },
+  {path :'industries', component: IndustriesComponent },
+  {path : 'digital' , component : DigitalComponent },
+  {path : 'contact' , component : ContactComponent },
+  {path : 'leadership', component:LeadershipComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { IndustriesComponent } from './industries/industries.component';
+import { DigitalComponent } from './digital/digital.component';
+import { ContactComponent } from './contact/contact.component';
+import { LeadershipComponent } from './leadership/leadership.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServiceComponent,
+    IndustriesComponent,
+    DigitalComponent,
+    ContactComponent,
+    LeadershipComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
